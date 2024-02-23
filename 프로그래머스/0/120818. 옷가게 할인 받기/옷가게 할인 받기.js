2def41779d88d0b33c697a4d7264f79d8ab802a2
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-02-24 08:04:30 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,14 +1,22 @@
 function solution(price) {
-    var discoutRate = 0;
-    
-    if(price >= 500000){
-        discoutRate = 0.2;
-    }else if(price >= 300000){
-        discoutRate = 0.1;
-    }else if(price >= 100000){
-        discoutRate = 0.05;
+     let discountRate = 0;
+
+    // 50만원 이상일때 할인율은 20%
+    if (price >= 500000) {
+        discountRate = 0.2;
+    // 30만원 이상일때 할인율은 10%
+    } else if (price >= 300000) {
+        discountRate = 0.1;
+    // 10만원 이상일때 할인율은 5%
+    } else if (price >= 100000) {
+        discountRate = 0.05;
     }
-    
-    var discountMoney = price * ( 1 - discoutRate);
-    return Math.floor(discountMoney);
+
+    // 총 할인된 금액
+    const discountedPrice = price * (1 - discountRate);
+
+    // 버림 연산을 사용하여 소수점 이하를 버린 정수를 얻음
+    const finalPrice = Math.floor(discountedPrice);
+
+    return finalPrice;
 }
