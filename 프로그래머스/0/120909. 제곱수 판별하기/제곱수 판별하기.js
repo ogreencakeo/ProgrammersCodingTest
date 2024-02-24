ca566e3cf25d3da800089cc3f875e7b13cca1478
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-02-24 21:53:30 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,5 +1,8 @@
 function solution(n) {
+    // Math.sqrt()는 제곱근을 계산해준다.
     var answer = Math.sqrt(n);
+    //  제곱수인 경우, 제곱근은 정수가 되므로 answer과
+    // Math.floor(answer)은 동일한 값
     if(answer === Math.floor(answer)){
         return 1;
     }else{
