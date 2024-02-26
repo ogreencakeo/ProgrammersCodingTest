23d045620fdc5a01bfe6ff51a5340bd9d7675eab
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-02-27 01:36:15 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,13 +1,13 @@
 function solution(num_list) {
     let multiplication = 1;
-    let sum_square1 = 0;
+    let sum_square = 0;
     for(let i of num_list){
         // 모든 원소들의 곱
         multiplication *= i;
         // 모든 원소들의 합
-        sum_square1 += i;
+        sum_square += i;
     }
     // 모든 원소들의 합의 제곱
-    let sum_square2 = Math.pow(sum_square1, 2);
-    return multiplication> sum_square2? 0 : 1;
+
+    return multiplication> Math.pow(sum_square, 2)? 0 : 1;
 }
