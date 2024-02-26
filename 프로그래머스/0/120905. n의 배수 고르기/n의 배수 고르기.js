1b1d4e56abd62387105d9fef6b3be1fde50b9658
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-02-27 00:29:40 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,6 +1,8 @@
 function solution(n, numlist) {
     let answer = [];
     for(let i of numlist){
+        // i의 값이 n의 배수일때 빈 배열 answer에다가
+        // i의 값을 push 해줌.
         if(i % n===0){
             answer.push(i);
         }
