33d5cf2d27abc5d49e7e96ab68a8e7a07c13aff0
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-02-28 00:56:44 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 1.96 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,5 +1,8 @@
 function solution(my_string, is_prefix) {
+    // slice를 사용하여 해당 접미사를 자름
     var answer = my_string.slice(0, is_prefix.length);
     console.log(answer);
+    
+    // 접미사와 매개변수 is_prefix값이 같으면 1을 리턴, 아니면 0을 리턴함
     return is_prefix===answer ? 1 : 0;
 }
