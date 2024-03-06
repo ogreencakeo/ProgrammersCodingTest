58e8f1ba689988d648e6eeae598f1579d6840991
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-03-07 00:20:06 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -1,11 +1,7 @@
 function solution(a, b) {
-    let answer1;
-    let answer2;
     let result;
     if(a%2==1 && b%2==1){
-        answer1 = Math.pow(a,2);
-        answer2 = Math.pow(b,2);
-        result = answer1 + answer2;
+        result = Math.pow(a,2) + Math.pow(b,2);
     }else if(a%2==1 || b%2==1){
         result = 2 * (a + b);
     }else if(!(a%2==1 && b%2==1)){
