4174b2dee829396797fc8a0dd9531d7b5b070d67
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-03-09 15:24:07 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,5 +1,5 @@
 function solution(my_string) {
-    let arr = my_string.replace(/\s+/g + ' ').trim();
-    let result = arr.match(/\S+/g);
+    let result = my_string.split(' ').filter(v => v);
     return result;
+    
 }
