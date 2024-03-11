6fac978c4ca952e36913c4672e3b04e1e2de919b
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-03-11 21:24:32 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,17 +1,8 @@
 function solution(num) {
     let sum = 0;
-    while(num !== 1){
-        if(num%2===0){
-            num/=2;
-            sum++;
-        }else{
-            num = num * 3 + 1;
-            sum++;
-        }
-        if(sum >= 500){
-            return -1;
-        }
+    while(num !== 1 && sum!==500){
+        num%2===0 ? num/=2 : num = num*3+1;
+        sum++;
     }
-    
-    return sum;
+    return num == 1?  sum : -1;
 }
