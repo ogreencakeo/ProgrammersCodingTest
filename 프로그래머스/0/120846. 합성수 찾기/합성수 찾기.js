87cfca211d5e0f7955fa2e6f7055092d917dcb27
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-03-11 19:18:21 +0900

    [level 0] Title: 합성수 찾기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,12 +1,14 @@
 function solution(n) {
-  let arr = [];
-  
-  for (let i = 1; i <= n; i ++) {
-    for (let j = 2; j < i; j++) {
-      if (i % j === 0) {
-        arr.push(i)
-      }
+    let arr = [];
+    
+    for(let i=1; i<=n; i++){
+        for(let j=2; j<i; j++){
+           if(i % j===0){
+               arr.push(i);
+           }
+        }
     }
-  }
-   return [...new Set(arr)].length
+    
+    // 중복값을 제서하기 위해 set()을 사용함.
+    return [...new Set(arr)].length;
 }
