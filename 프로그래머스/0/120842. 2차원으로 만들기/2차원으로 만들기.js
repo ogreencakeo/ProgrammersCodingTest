873d8fe48ac1168961174984c5a81cef2816245d
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-03-12 20:58:33 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,7 +1,9 @@
 function solution(num_list, n) {
-    let arr = [];
-    for(let i=0; i<num_list.length/n; i++){
-        arr.push(num_list.slice(i*n, i*n+n));
+    let answer = [];
+
+    for (let i = 0; i<num_list.length; i=i+n){
+        answer.push(num_list.slice(i, i+n));
     }
-    return arr;
+
+    return answer;
 }
