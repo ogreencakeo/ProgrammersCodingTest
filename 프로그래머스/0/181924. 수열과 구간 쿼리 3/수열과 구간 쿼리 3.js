9d0703fe2085ca0c0e2b0d0cf8868d749055dfb4
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-03-13 20:54:14 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -0,0 +1,9 @@
+function solution(arr, queries) {
+    for(let i=0; i<queries.length; i++){
+        const [a, b] = queries[i];
+        let temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+    return arr;
+}
