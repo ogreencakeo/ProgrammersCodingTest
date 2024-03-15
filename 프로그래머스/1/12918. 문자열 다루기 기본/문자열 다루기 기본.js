6073efa7177ec7b61c9104ff936f6b6211ede42b
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-03-15 19:07:46 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,13 +1,13 @@
 function solution(s) {
-   
     let arr = s.split('');
-    if(arr.length !== 4 && arr.length !== 6){
+    
+    if(arr.length !== 4 && arr.length!==6){
         return false;
     }
-         
-    for(let i=0; i<arr.length; i++){
-        if(isNaN(parseInt(arr[i]))){
-           return false; 
+    
+    for(let i of arr){
+        if(isNaN(parseInt(i))){
+            return false;
         }
     }
     return true;
