5dd170721de809817c1df8cb7fa283699f09387d
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-03-15 18:33:59 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.31 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,5 +1,6 @@
 function solution(left, right) {
     let result = 0;
+    
     for(let i=left; i<=right; i++){
         let cnt = 0;
         for(let j=1; j<=i; j++){
@@ -7,15 +8,12 @@ function solution(left, right) {
                 cnt++;
             }
         }
-        // 홀 짝
-        if(cnt % 2 === 0){
+        if(cnt%2 === 0){
             result += i;
         }else{
             result -= i;
         }
-        
+       
     }
-    
-    return result;
-  
+     return result;
 }
