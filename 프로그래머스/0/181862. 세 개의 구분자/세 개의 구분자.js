811fdb2517b69d5b2237b7a8fcc1c981260b3abf
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-03-15 17:21:27 +0900

    [level 0] Title: 세 개의 구분자, Time: 19.25 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,26 +1,4 @@
 function solution(myStr) {
-    let str = [];
-    let temp = '';
-    
-    for(let i of myStr){
-        if(i === 'a' || i === 'b' || i=== 'c'){
-            if(temp.length > 0){
-                str.push(temp);
-                temp = '';
-            }
-        }else{
-            temp += i;
-        }
-        
-    }
-    
-    if(temp.length > 0){
-        str.push(temp);
-    }
-    
-    if(str.length === 0){
-        return ['EMPTY'];
-    }else{
-        return str;
-    }
+    const list = myStr.split(/[a | b | c]/).filter(v => v);
+    return list.length? list : ['EMPTY'];
 }
