526a3d662f48702b3cdb135597380971edb43c46
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-03-16 18:51:10 +0900

    [level 0] Title: qr code, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.js
+++ 프로그래머스/0/181903. qr code/qr code.js
@@ -2,10 +2,7 @@ function solution(q, r, code) {
     let arr = code.split('');
     let str = '';
     for(let i=r; i<arr.length; i+=q){
-        // if(i % q === 0){
-            // console.log(i)
-            str += arr[i];
-        // }
+        str += arr[i];
     }
     return str;
 }
