56977ea9df81c824149122a13de610ea311ad846
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-03-16 18:28:00 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -2,11 +2,7 @@ function solution(s) {
     let arr = s.split(' ');
     let sum = 0;
     for(let i=0; i<arr.length; i++){
-        if(arr[i] === 'Z'){
-            sum-= parseInt(arr[i-1]);
-        }else{
-            sum+= parseInt(arr[i]);
-        }
+        arr[i] === 'Z' ? sum-= parseInt(arr[i-1]) : sum+= parseInt(arr[i]);
     }
     return sum;
 }
