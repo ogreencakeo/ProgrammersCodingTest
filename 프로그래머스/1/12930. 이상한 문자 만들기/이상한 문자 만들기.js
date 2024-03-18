8ffdd07ecfc8cd4fa3dc7837c0b6e36a6b2a7e5a
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-03-18 21:05:04 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 3.54 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,15 +1,18 @@
 function solution(s) {
-    let answer = s.split(' ');
+    
+    let arr = s.split(' ');
+    console.log(arr);
     let result = [];
-    //return answer;
-    for(let i of answer){
+    
+    for(let i of arr){
         for(let j=0; j<i.length; j++){
-            j%2 === 0?  result.push(i[j].toUpperCase()) : result.push(i[j].toLowerCase());
+            j%2 === 0? result.push(i[j].toUpperCase()) : result.push(i[j].toLowerCase());
         }
         result.push(' ');
     }
+    
     // 마지막 공백 제거
-    result.pop(' ');
+    result.pop(' ')
     
     return result.join('');
 }
