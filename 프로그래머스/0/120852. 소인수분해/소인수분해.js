01f92b9d58abd8e49c7cc2fa6e7c3b56f0275544
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-03-18 20:05:50 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -1,15 +1,15 @@
 function solution(n) {
     // 소인수를 저장할 배열
     let result = [];
+    // 1은 소인수에 해당 되지 않으니까 2부터 시작함
     let divisor = 2;
-    
     while(n > 1){
         if(n%divisor === 0){
             result.push(divisor);
             n /= divisor;
         }else{
             divisor++;
-        }   
+        }
     }
-    return [...new Set(result)];
+    return [... new Set(result)];
 }
