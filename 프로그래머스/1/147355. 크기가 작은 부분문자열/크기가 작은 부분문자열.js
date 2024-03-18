3b25fc7b27b1b15312dd7098a0e711b63abdd696
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-03-18 21:24:35 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,11 +1,9 @@
 function solution(t, p) {
     let cnt = 0;
     let pNum = parseInt(p);
-   
     for(let i=0; i<t.length - p.length + 1; i++){
         const subString = t.substring(i, i+p.length);
-        const subNum = parseInt(subString);
-        if(subNum <= pNum){
+        if(subString <= pNum){
             cnt++;
         }
     }
