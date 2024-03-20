b20af65c7ddb5d94f26f07edf2ef6c8cb0f6ec45
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-03-20 18:40:56 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    let numArr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    
+    numArr.forEach((v, i) => {
+        numbers = numbers.replaceAll(v, i);
+    });
+    
+    return Number(numbers);
+}
