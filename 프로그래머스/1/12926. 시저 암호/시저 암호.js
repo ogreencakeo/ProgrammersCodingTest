f8c84b550ff639cf757349b2df0b0601bae76c4a
ogreencakeo <116356522+ogreencakeo@users.noreply.github.com>
2024-03-22 23:29:18 +0900

    [level 1] Title: 시저 암호, Time: 4.98 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,19 +1,22 @@
 function solution(s, n) {
-    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lower = "abcdefghijklmnopqrstuvwxyz";
-    var answer= '';
-
-    for(var i =0; i <s.length; i++){
-        var text = s[i];
-        if(text == ' ') {
-            answer += ' '; 
+    let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let lower = "abcdefghijklmnopqrstuvwxyz";
+    let answer = '';
+    
+    for(let i=0; i<s.length; i++){
+        let text = s[i];
+        
+        if(text === ' '){
+            answer += ' ';
             continue;
         }
-        var textArr = upper.includes(text) ? upper : lower;
-        var index = textArr.indexOf(text)+n;
+        
+        let textArr = upper.includes(text) ? upper : lower;
+        let idx = textArr.indexOf(text) + n;
+        
         // 새로운 인덱스가 알파벳 문자열의 길이를 넘어가면 순환시키기 위해, 길이 만큼을 빼줌.
-        if(index >= textArr.length) index -= textArr.length;
-        answer += textArr[index];
+        if(idx >= textArr.length) idx -= textArr.length;
+        answer += textArr[idx];
     }
     return answer;
 }
