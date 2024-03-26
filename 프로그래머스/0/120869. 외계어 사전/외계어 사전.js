cb05cd7988e9d8316d2f2b90a8ca86204e4586c3
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-03-26 21:39:47 +0900

    [level 0] Title: 외계어 사전, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
@@ -1,13 +1,3 @@
 function solution(spell, dic) {
-    const result = [];
-    dic.forEach((v) => {
-       let count = 0;
-        spell.forEach((j) => {
-            if(v.includes(j)) count++;
-        });
-        
-        if(count === spell.length) result.push(v);
-    });
-    
-    return result.length === 0? 2 : 1;
+    return dic.filter((v) => spell.every((c) => v.includes(c))).length ? 1 : 2;
 }
