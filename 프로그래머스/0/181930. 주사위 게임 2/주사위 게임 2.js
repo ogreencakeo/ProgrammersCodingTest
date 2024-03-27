4c66882af568247a72f51b56ae2ff2d207102e4b
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-03-28 00:07:00 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,15 +1,16 @@
 function solution(a, b, c) {
-    const covert = {
-        'case1': (a, b, c) => a + b + c,
-        'case2': (a, b, c) => (a + b + c) * (a**2 + b**2 + c**2),
-        'case3': (a, b, c) => (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3),
-    };
-
-    if (a === b && b === c) {
-        return covert['case3'](a, b, c);
-    } else if (a === b || b === c || c === a) {
-        return covert['case2'](a, b, c);
-    } else {
-        return covert['case1'](a, b, c);
+    const set = new Set([a, b, c]);
+    switch([...set].length){
+        case 1 : return calculate([a, b, c], 3);
+        case 2 : return calculate([a, b, c], 2);
+        case 3 : return calculate([a, b, c], 1);
     }
+}
+function calculate(fn, n=1){
+    const [a, b, c] = fn;
+    let result = 1;
+    for(let i=1; i<=n; i++){
+        result *= Math.pow(a, i) + Math.pow(b, i) + Math.pow(c, i);
+    }
+    return result;
 }
