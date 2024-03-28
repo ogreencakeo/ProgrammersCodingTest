96d710f9773e023d7e9ca426df4b186ac81300bd
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-03-29 00:47:07 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,9 +1,14 @@
 function solution(numbers) {
-    let numArr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let numArr = [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let result = "";
+    let temp = "";
     
-    numArr.forEach((v, i) => {
-        numbers = numbers.replaceAll(v, i);
-    });
-    
-    return Number(numbers);
+    for(let i=0; i<numbers.length; i++){
+        temp += numbers[i];
+        if(numArr.includes(temp)){
+            result += numArr.indexOf(temp);
+            temp = "";
+        }
+    }
+    return parseInt(result);
 }
