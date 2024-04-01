1ce57a96f9ca321a46f88cb8e10e211f80af239d
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-04-02 02:21:53 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/README.md
+++ 프로그래머스/0/181905. 문자열 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 뒤집기 - 181905 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181905) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181905#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 4.84 ms
+메모리: 33.4 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 13일 20:20:47
+2024년 04월 02일 02:21:51
 
 ### 문제 설명
 
--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,5 +1,5 @@
 function solution(my_string, s, e) {
-    let arr = my_string.substring(s, e+1).split('').reverse();
-    console.log(arr);
-    return my_string.substr(0, s) + arr.join('')+ my_string.substr(e+1);
+    let arr = my_string.split('').slice(s, e+1).reverse().join('');
+    let answer = my_string.substring(0, s) + arr + my_string.substring(e+1);
+    return answer;
 }
